3dfc8079d0185d161a7f5deb0e1caa73b07ce765
Matt <matthias.bultje@topicus.nl>
2020-12-10 17:25:22 +0100

    New test for public subjects

--- libs/tests/src/lib/assignment2.spec.ts
+++ libs/tests/src/lib/assignment2.spec.ts
@@ -1,4 +1,16 @@
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+class Counter {
+  public valueSubject: BehaviorSubject<number>;
+
+  constructor() {
+    this.valueSubject = new BehaviorSubject<number>(0);
+  }
+
+  public count() {
+    this.valueSubject.next(this.valueSubject.getValue() + 1);
+  }
+}
 
 describe('assignment 2', () => {
   test('subjects are broadcasting', () => {
@@ -13,4 +25,15 @@ describe('assignment 2', () => {
     subject.next(2);
     subject.next(3);
   });
+
+  test('subjects should not be public', () => {
+    const counter = new Counter();
+
+    counter.valueSubject.subscribe(count => console.log(count)); //0
+
+    counter.count(); //1
+    counter.count(); //2
+    counter.valueSubject.next(42);
+    counter.count(); //3?
+  })
 });
