ae51bdfc43dbb19db8dd87cc554abaa972376688
Matt <matthias.bultje@topicus.nl>
2020-12-21 10:06:05 +0100

    Switchmap cases

--- libs/tests/src/lib/pipe-operators/switch-map.spec.ts
+++ libs/tests/src/lib/pipe-operators/switch-map.spec.ts
@@ -1,13 +1,14 @@
-import { of } from 'rxjs';
-import { delay, switchMap } from 'rxjs/operators';
+import { of, timer } from 'rxjs';
+import { delay, finalize, switchMap, tap } from 'rxjs/operators';
 import { tapLog } from 'rxjs-util/dist';
+import { cold } from 'jest-marbles';
 
 describe('switchMap', () => {
     test('should call second observable with results of first', done => {
-        const typeAhead = () => of(1,2,3)
+        const typeAhead = () => of(1, 2, 3)
             .pipe(
-            delay(100),
-            tapLog()
+                delay(100),
+                tapLog()
             );
 
         const getData = url => of(`data from '${url}'`)
@@ -21,4 +22,27 @@ describe('switchMap', () => {
         )
             .subscribe({ complete: done });
     });
+
+    test('should not complete in main pipe', done => {
+        cold('--1--')
+            .pipe(
+                switchMap(() => cold('-2-|')),
+                finalize(() => fail('should not complete'))
+            )
+            .subscribe();
+
+        timer(500).subscribe(done);
+    });
+
+    test('should set loading for second call', () => {
+        const setLoading = loading => console.log(`set loading to ${loading}`);
+
+        cold('-1-1-')
+            .pipe(
+                tap(() => setLoading(true)),
+                switchMap(() => of('data')
+                    .pipe(finalize(() => setLoading(false))))
+            )
+            .subscribe()
+    })
 });
